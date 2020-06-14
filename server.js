d0534707bff999daf405c8e1d513b6be9a08eabb
Cassio-2012 <cassio-2012@hotmail.com>
2020-06-14 19:51:20 -0300

    ajuste de portas

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = new Koa();
 const server = http.createServer(app.callback());
 const io = socket(server);
 
-const SERVER_HOST = "localhost";
+const SERVER_HOST = "https://serverchatjust.azurewebsites.net";
 const SERVER_PORT = 8000;
 
 io.on("connection", (socket) => {
