7640ab29177349d6578d47b7afc32df94dbdc25a
Cassio-2012 <cassio-2012@hotmail.com>
2020-06-15 14:47:29 -0300

    ajuste porta

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = new Koa();
 const server = http.createServer(app.callback());
 const io = socket(server);
 
-const SERVER_HOST = "serverchatjust.azurewebsites.net";
-const SERVER_PORT = 443;
+const SERVER_HOST = "localhost";
+const SERVER_PORT = 8000;
 
 io.on("connection", (socket) => {
   console.log("[IO] Connection => Server has a new connection");
