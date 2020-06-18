744e0804d3b29571e47cbbcb260ca17cad337935
Messias <messias.d.carvalho@hotmail.com>
2020-06-18 19:30:39 -0300

    vai

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const server = http.createServer(app.callback());
 const io = socket(server);
 
 const SERVER_HOST = "localhost";
-const SERVER_PORT = process.env.PORT || 8000;
-
+// const SERVER_PORT = process.env.PORT || 8000;
+const SERVER_PORT = "http://serverchatjust.azurewebsites.net";
 
 io.on("connection", (socket) => {
   console.log("[IO] Connection => Server has a new connection");
